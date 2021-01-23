79f58ab72e20e8417ed99c32d2be2cdae6e1e962
Anas Tasadduq <anas.tasadduq@gmail.com>
2021-01-24 00:44:48 +0300

    adjust chart axes, add channel title

--- eeglab/lib/widgets/MyChart.dart
+++ eeglab/lib/widgets/MyChart.dart
@@ -4,10 +4,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class MyChart extends StatefulWidget {
-  const MyChart(this.list, this.channel, {Key key}) : super(key: key);
+  const MyChart(this.list, this.channel, this.xAxis, this.channelName, {Key key})
+      : super(key: key);
 
   final List<EEGData> list;
   final int channel;
+  final bool xAxis;
+  final String channelName;
 
   @override
   State<StatefulWidget> createState() => _MyChartState();
@@ -26,15 +29,34 @@ class _MyChartState extends State<MyChart> {
   Widget build(BuildContext context) {
     return GestureDetector(
       behavior: HitTestBehavior.translucent,
-      child: SfCartesianChart(
-        tooltipBehavior: TooltipBehavior(enable: true),
-        primaryXAxis: CategoryAxis(),
-        series: <ChartSeries>[
-          LineSeries<EEGData, String>(
-              enableTooltip: true,
-              dataSource: _list,
-              xValueMapper: (EEGData data, _) => data.time,
-              yValueMapper: (EEGData data, _) => data.data[widget.channel])
+      child: Row(
+        children: [
+          Text(widget.channelName),
+          Expanded(
+            child: SfCartesianChart(
+              tooltipBehavior: TooltipBehavior(enable: true),
+              primaryXAxis: CategoryAxis(
+                isVisible: widget.xAxis,
+                // interval: 5,
+              ),
+              primaryYAxis: NumericAxis(
+                maximum: 1.7,
+                minimum: -1.7,
+                interval: 1,
+                // title: AxisTitle(
+                //   text: 'Channel Y',
+                // ),
+              ),
+              series: <ChartSeries>[
+                LineSeries<EEGData, String>(
+                    enableTooltip: true,
+                    dataSource: _list,
+                    xValueMapper: (EEGData data, _) => data.time,
+                    yValueMapper: (EEGData data, _) =>
+                        data.data[widget.channel])
+              ],
+            ),
+          ),
         ],
       ),
       onTap: () => Navigator.of(context)
