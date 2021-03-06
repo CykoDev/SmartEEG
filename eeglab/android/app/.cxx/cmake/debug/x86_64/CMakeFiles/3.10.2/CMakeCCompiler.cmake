e95fcc926b8fff3efdee0b5d75419a6511bec8b9
Anas Tasadduq <anas.tasadduq@gmail.com>
2021-03-06 18:15:35 +0500

    request data from flask server via https requests

--- eeglab/android/app/.cxx/cmake/debug/x86_64/CMakeFiles/3.10.2/CMakeCCompiler.cmake
+++ eeglab/android/app/.cxx/cmake/debug/x86_64/CMakeFiles/3.10.2/CMakeCCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_C_COMPILER "C:/Users/muham/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/bin/clang.exe")
+set(CMAKE_C_COMPILER "C:/Users/anast/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/bin/clang.exe")
 set(CMAKE_C_COMPILER_ARG1 "")
 set(CMAKE_C_COMPILER_ID "Clang")
 set(CMAKE_C_COMPILER_VERSION "11.0.5")
@@ -16,9 +16,9 @@ set(CMAKE_C_SIMULATE_VERSION "")
 
 
 
-set(CMAKE_AR "C:/Users/muham/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-ar.exe")
+set(CMAKE_AR "C:/Users/anast/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-ar.exe")
 set(CMAKE_C_COMPILER_AR "CMAKE_C_COMPILER_AR-NOTFOUND")
-set(CMAKE_RANLIB "C:/Users/muham/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-ranlib.exe")
+set(CMAKE_RANLIB "C:/Users/anast/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-ranlib.exe")
 set(CMAKE_C_COMPILER_RANLIB "CMAKE_C_COMPILER_RANLIB-NOTFOUND")
 set(CMAKE_LINKER "CMAKE_LINKER-NOTFOUND")
 set(CMAKE_COMPILER_IS_GNUCC )
@@ -69,5 +69,5 @@ endif()
 
 
 set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "gcc;dl;c;gcc;dl")
-set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "C:/Users/muham/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/lib64/clang/11.0.5/lib/linux/x86_64;C:/Users/muham/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/lib/gcc/x86_64-linux-android/4.9.x;C:/Users/muham/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/x86_64-linux-android/lib64;C:/Users/muham/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib/x86_64-linux-android/21;C:/Users/muham/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib/x86_64-linux-android;C:/Users/muham/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/x86_64-linux-android/lib;C:/Users/muham/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib")
+set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "C:/Users/anast/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/lib64/clang/11.0.5/lib/linux/x86_64;C:/Users/anast/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/lib/gcc/x86_64-linux-android/4.9.x;C:/Users/anast/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/x86_64-linux-android/lib64;C:/Users/anast/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib/x86_64-linux-android/21;C:/Users/anast/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib/x86_64-linux-android;C:/Users/anast/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/x86_64-linux-android/lib;C:/Users/anast/AppData/Local/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib")
 set(CMAKE_C_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
--- eeglab/lib/screens/heatmap_screen.dart
+++ eeglab/lib/screens/heatmap_screen.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import 'settings_screen.dart';
 import 'signal_data_screen.dart';
 
-class HeatmapScreen extends StatelessWidget {
+class HeatmapScreen extends StatefulWidget {
   static String routeName = '/heatmap';
+
+  @override
+  _HeatmapScreenState createState() => _HeatmapScreenState();
+}
+
+class _HeatmapScreenState extends State<HeatmapScreen> {
+  String _outputString = 'File Format';
+
+  void fetchCSV() async {
+    final response = await http.get(
+      Uri.https('smart-eeg.herokuapp.com', 'conversion/test'),
+    );
+
+    int code = response.statusCode;
+    print(code);
+
+    _outputString = response.body;
+  }
+
+  void changeState() async {
+    // final scaffold = Scaffold.of(context);
+    // scaffold.showSnackBar(
+    //   SnackBar(
+    //     content: const Text('Getting data, please wait'),
+    //     action: SnackBarAction(
+    //         label: 'OK', onPressed: scaffold.hideCurrentSnackBar),
+    //   ),
+    // );
+    await fetchCSV();
+    setState(() {
+      _outputString = _outputString;
+    });
+    // scaffold.showSnackBar(
+    //   SnackBar(
+    //     content: const Text('Data received!'),
+    //     action: SnackBarAction(
+    //         label: 'OK', onPressed: scaffold.hideCurrentSnackBar),
+    //   ),
+    // );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,13 +72,19 @@ class HeatmapScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: new Text(
-          'File Format',
-          style: TextStyle(
-            fontSize: 30,
+      body: Column(
+        children: [
+          Text(
+            _outputString,
+            style: TextStyle(
+              fontSize: 30,
+            ),
           ),
-        ),
+          RaisedButton(
+            child: Text('Get Data'),
+            onPressed: changeState,
+          ),
+        ],
       ),
     );
   }
